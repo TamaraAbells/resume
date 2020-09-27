2b39be384789feb4f9a9a5a6c72b07a8bafefe6c
danbeneventano <danbeneventano@protonmail.com>
2020-09-26 18:59:08 -0600

    Fix purgeCSS

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,15 +1,7 @@
-const path = require('path')
-
 module.exports = {
   experimental: 'all',
   future: {
     removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true
-  },
-  purge: {
-    content (defaultContents) {
-      return defaultContents
-        .map(p => path.join('src/', p))
-    }
   }
 }
