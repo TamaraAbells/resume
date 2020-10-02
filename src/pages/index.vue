6d4fc5142ad571c8d2c9ee6e8047100e9327c744
danbeneventano <danbeneventano@protonmail.com>
2020-10-02 15:26:33 -0600

    Add noopener to links, use properly sized webp for headshot pic, add alt text to headshot img

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,7 +2,7 @@
   <div class="sheet">
     <div class="grid grid-cols-12 gap-6">
       <div class="flex flex-row col-span-8">
-        <img src="~/assets/headshot.jpg" class="object-cover w-32 h-32 border-2 border-indigo-600 rounded-full">
+        <img src="~/assets/headshot.webp" alt="Headshot Photo" class="object-cover w-32 h-32 border-2 border-indigo-600 rounded-full">
         <div class="flex flex-col ml-12">
           <div class="text-2xl font-medium">
             Dan Beneventano
@@ -10,22 +10,22 @@
           <div class="mb-1 text-gray-700 text-md">
             Full Stack Software Engineer
           </div>
-          <a class="altlink" href="https://www.github.com/danbeneventano" target="_blank">
+          <a class="altlink" href="https://www.github.com/danbeneventano" target="_blank" rel="noopener">
             <img src="~/assets/logos/github-brands.svg" class="w-4 h-4" alt="GitHub logo">
             <div class="ml-4">github.com/danbeneventano</div>
           </a>
-          <a class="altlink" href="https://danbeneventano.me" target="_blank">
+          <a class="altlink" href="https://danbeneventano.me" target="_blank" rel="noopener">
             <img src="~/assets/logos/link-solid.svg" class="w-3 h-3" alt="Link icon">
             <div class="ml-5">danbeneventano.me</div>
           </a>
         </div>
       </div>
       <div class="flex flex-col items-end col-span-4">
-        <a class="link" href="mailto:danbeneventano@gmail.com">
+        <a class="link" href="mailto:danbeneventano@gmail.com" rel="noopener">
           <img src="~/assets/logos/envelope-solid.svg" class="w-3 h-3" alt="Email icon">
           <div class="ml-4">danbeneventano@gmail.com</div>
         </a>
-        <a class="link" href="tel:+1201-919-1125">
+        <a class="link" href="tel:+1201-919-1125" rel="noopener">
           <img src="~/assets/logos/phone-alt-solid.svg" class="w-3 h-3" alt="Phone icon">
           <div class="ml-4">+1 (201) 919-1125</div>
         </a>
@@ -42,21 +42,21 @@
           class="flex flex-col"
         >
           <div class="flex flex-row items-end">
-            <a href="https://cometx.io" target="_blank">
+            <a href="https://cometx.io" target="_blank" rel="noopener">
               <img src="~/assets/logos/cometx_logo.svg" class="h-4 m-1" alt="CometX logo">
             </a>
-            <span class="ml-3 text-xs text-gray-700">July 2019 – Present &middot; <a href="https://cometx.io" target="_blank" class="text-indigo-600 hover:underline">Link</a> &middot; <a href="https://github.com/comet-app/cometx" target="_blank" class="text-indigo-600 hover:underline">GitHub</a></span>
+            <span class="ml-3 text-xs text-gray-700">July 2019 – Present &middot; <a href="https://cometx.io" target="_blank" rel="noopener" class="text-indigo-600 hover:underline">Link</a> &middot; <a href="https://github.com/comet-app/cometx" target="_blank" rel="noopener" class="text-indigo-600 hover:underline">GitHub</a></span>
           </div>
           <span class="mt-1 text-sm italic font-semibold text-gray-700">Co-Founder & Lead Software Engineer</span>
           <div class="mt-1 text-sm text-gray-700">
             <p>
-              <a href="https://cometx.io" target="_blank" class="hover:underline">CometX.io</a> is a new and growing platform for creating online communities and sharing posts and comments. CometX launched in August 2020, and has already amassed over 500 daily users.
+              <a href="https://cometx.io" target="_blank" rel="noopener" class="hover:underline">CometX.io</a> is a new and growing platform for creating online communities and sharing posts and comments. CometX launched in August 2020, and has already amassed over 500 daily users.
             </p>
             <p class="mt-1">
-              <a href="https://cometx.io" target="_blank" class="hover:underline">CometX.io</a> uses Vue.js in the frontend, Node.js, GraphQL and SQL in the backend, and Docker for deployment.
+              <a href="https://cometx.io" target="_blank" rel="noopener" class="hover:underline">CometX.io</a> uses Vue.js in the frontend, Node.js, GraphQL and SQL in the backend, and Docker for deployment.
             </p>
             <p class="mt-1">
-              I programmed the entire <a href="https://cometx.io" target="_blank" class="hover:underline">CometX.io</a> platform (frontend and backend), automated deployment, and grew a dedicated community. <a href="https://cometx.io" target="_blank" class="hover:underline">CometX.io</a> is open source and the code is available on <a class="text-indigo-500 hover:underline" href="https://github.com/comet-app/cometx" target="_blank">our GitHub</a>.
+              I programmed the entire <a href="https://cometx.io" target="_blank" rel="noopener" class="hover:underline">CometX.io</a> platform (frontend and backend), automated deployment, and grew a dedicated community. <a href="https://cometx.io" target="_blank" rel="noopener" class="hover:underline">CometX.io</a> is open source and the code is available on <a class="text-indigo-600 hover:underline" href="https://github.com/comet-app/cometx" target="_blank" rel="noopener">our GitHub</a>.
             </p>
           </div>
         </div>
@@ -84,8 +84,8 @@
           class="flex flex-col"
         >
           <span>
-            <a href="https://devpost.com/software/hand-of-god-tdjxr8" target="_blank" class="font-medium">Hand of God</a>
-            <span class="ml-1 text-xs text-gray-700">February 2020 &middot; <a href="https://devpost.com/software/hand-of-god-tdjxr8" target="_blank" class="text-indigo-600 hover:underline">Link</a> &middot; <a href="https://github.com/The-Hand-of-God" target="_blank" class="text-indigo-600 hover:underline">GitHub</a></span>
+            <a href="https://devpost.com/software/hand-of-god-tdjxr8" target="_blank" rel="noopener" class="font-medium">Hand of God</a>
+            <span class="ml-1 text-xs text-gray-700">February 2020 &middot; <a href="https://devpost.com/software/hand-of-god-tdjxr8" target="_blank" rel="noopener" class="text-indigo-600 hover:underline">Link</a> &middot; <a href="https://github.com/The-Hand-of-God" target="_blank" rel="noopener" class="text-indigo-600 hover:underline">GitHub</a></span>
           </span>
           <span class="mt-1 text-sm italic text-gray-700">Built at HackCU 2020 – Second place winner</span>
           <div class="mt-1 text-sm text-gray-700">
@@ -102,13 +102,13 @@
           class="flex flex-col mt-3"
         >
           <span>
-            <a href="https://github.com/danbeneventano/resume" target="_blank" class="font-medium">This Resume</a>
-            <span class="ml-1 text-xs text-gray-700">September 2020 &middot; <a href="https://resume.danbeneventano.me" target="_blank" class="text-indigo-600 hover:underline">Link</a> &middot; <a href="https://github.com/danbeneventano/resume" target="_blank" class="text-indigo-600 hover:underline">GitHub</a></span>
+            <a href="https://github.com/danbeneventano/resume" target="_blank" rel="noopener" class="font-medium">This Resume</a>
+            <span class="ml-1 text-xs text-gray-700">September 2020 &middot; <a href="https://resume.danbeneventano.me" target="_blank" rel="noopener" class="text-indigo-600 hover:underline">Link</a> &middot; <a href="https://github.com/danbeneventano/resume" target="_blank" rel="noopener" class="text-indigo-600 hover:underline">GitHub</a></span>
           </span>
           <span class="mt-1 text-sm italic text-gray-700">This resume is actually a website!</span>
           <div class="mt-1 text-sm text-gray-700">
             <p>
-              In order to exercise my design skills and create a great resume, I programmed this resume as a website using Vue.js and TailwindCSS. The resume is available online at <a href="https://resume.danbeneventano.me" target="_blank" class="text-indigo-600 hover:underline">danbeneventano.me</a>, and is open source on <a href="https://github.com/danbeneventano/resume" target="_blank" class="text-indigo-600 hover:underline">GitHub</a>!
+              In order to exercise my design skills and create a great resume, I programmed this resume as a website using Vue.js and TailwindCSS. The resume is available online at <a href="https://resume.danbeneventano.me" target="_blank" rel="noopener" class="text-indigo-600 hover:underline">danbeneventano.me</a>, and is open source on <a href="https://github.com/danbeneventano/resume" target="_blank" rel="noopener" class="text-indigo-600 hover:underline">GitHub</a>!
             </p>
           </div>
         </div>
@@ -122,7 +122,7 @@
           class="flex flex-col"
         >
           <div class="flex flex-row items-end">
-            <a href="https://www.mines.edu" target="_blank">
+            <a href="https://www.mines.edu" target="_blank" rel="noopener">
               <img src="~/assets/logos/mines.png" class="h-10" alt="Colorado School of Mines logo">
             </a>
           </div>
